a2b6462b7df74d2d1e22ab5543bcf718b4d85aa8
Shriram Sekar <shriramsekar11@gmail.com>
2022-07-29 10:56:24 +0400

    feat: implement quitting game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Restart");
+use std::io::{stdout, Write};
+use crossterm::{execute, Result, terminal::{EnterAlternateScreen, LeaveAlternateScreen, enable_raw_mode, disable_raw_mode}, event::{read, Event, KeyCode, KeyEvent}};
+
+fn main() -> Result<()> {
+    let mut stdout = stdout();
+
+    //setup
+    enable_raw_mode()?;
+    execute!(stdout, EnterAlternateScreen)?;
+
+    //main game loop
+    'gameloop: loop {
+        while let Event::Key(KeyEvent {code, .. }) = read()? {
+            match code {
+                //q to exit
+                KeyCode::Char(q) => break 'gameloop,
+                _ => continue,
+            }
+        }
+    }
+
+    //cleanup
+    execute!(stdout, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+
+    return Result::Ok(());
+    
 }
